fed712b09213cee63f02386a45b2203344c1e0de
Mohammad <moh.hossain98@gmail.com>
2024-01-04 14:52:49 -0500

    grid section added with challenges

--- index.js
+++ index.js
@@ -12,3 +12,23 @@ dropdownButton.forEach((button) => {
     button.textContent = button.textContent === "➡️" ? "⬇️" : "➡️";
   });
 });
+
+let flexButton = document.querySelector(".flex-button");
+let flexContainer = document.querySelector(".items");
+let flexPractice = document.querySelector(".flex-practice");
+flexButton.addEventListener("click", function () {
+  flexContainer.classList.toggle("visible");
+  gridContainer.classList.remove("visible");
+  flexButton.classList.toggle("active");
+  gridButton.classList.remove("active");
+  flexPractice.classList.toggle("hidden");
+});
+
+let gridButton = document.querySelector(".grid-button");
+let gridContainer = document.querySelector(".wrapper");
+gridButton.addEventListener("click", function () {
+  flexContainer.classList.remove("visible");
+  gridContainer.classList.toggle("visible");
+  gridButton.classList.toggle("active");
+  flexButton.classList.remove("active");
+});
